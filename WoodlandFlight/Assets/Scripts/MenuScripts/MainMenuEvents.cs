0939c1418e58b9535e7e9f8b88339bb9825cfdc8
CheesyRobot <ievalt2020@gmail.com>
2025-10-26 05:56:59 +0200

    Settings Menu implementacija
    Galima atsidaryti Settings meniu, peržiūrėti nustatymus. Kol kas tik vaizdas

--- WoodlandFlight/Assets/Scripts/MenuScripts/MainMenuEvents.cs
+++ WoodlandFlight/Assets/Scripts/MenuScripts/MainMenuEvents.cs
@@ -8,18 +8,40 @@ public class MainMenuEvents : MonoBehaviour
     private Button _playButton;
     private Button _settingsButton;
     private Button _quitButton;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private TemplateContainer _main;
+    private Button _returnButton;
+    private Button _gameButton;
+    private Button _videoButton;
+    private Button _audioButton;
+    private Button _controlsButton;
+    private TemplateContainer _settings;
     void Start()
     {
         _document = GetComponent<UIDocument>();
 
-        _playButton = _document.rootVisualElement.Q("PlayButton") as Button;
-        _settingsButton = _document.rootVisualElement.Q("SettingsButton") as Button;
-        _quitButton = _document.rootVisualElement.Q("QuitButton") as Button;
+        InitiateMainMenu();
+        InitiateSettingsMenu();
+        
+    }
+
+    private void InitiateMainMenu()
+    {
+        _main = _document.rootVisualElement.Q<TemplateContainer>("MainMenu");
+        _playButton = _main.Q<Button>("PlayButton");
+        _settingsButton = _main.Q<Button>("SettingsButton");
+        _quitButton = _main.Q<Button>("QuitButton");
 
         _playButton.RegisterCallback<ClickEvent>(OnPlayClick);
         _settingsButton.RegisterCallback<ClickEvent>(OnSettingsClick);
         _quitButton.RegisterCallback<ClickEvent>(OnQuitClick);
+    }
+
+    private void InitiateSettingsMenu()
+    {
+        _settings = _document.rootVisualElement.Q<TemplateContainer>("SettingsMenu");
+        _returnButton = _settings.Q<Button>("ReturnButton");
+
+        _returnButton.RegisterCallback<ClickEvent>(OnSettingsReturnClick);
     }
 
     private void OnDisable()
@@ -27,22 +49,29 @@ public class MainMenuEvents : MonoBehaviour
         _playButton.UnregisterCallback<ClickEvent>(OnPlayClick);
         _settingsButton.UnregisterCallback<ClickEvent>(OnSettingsClick);
         _quitButton.UnregisterCallback<ClickEvent>(OnQuitClick);
+        _returnButton.UnregisterCallback<ClickEvent>(OnSettingsReturnClick);
     }
 
     private void OnPlayClick(ClickEvent evt)
     {
-        //Debug.Log("Play");
         SceneManager.LoadScene(1);
     }
 
     private void OnSettingsClick(ClickEvent evt)
     {
-        Debug.Log("Settings");
+        _settings.style.display = DisplayStyle.Flex;
+        _main.style.display = DisplayStyle.None;
+    }
+
+    private void OnSettingsReturnClick(ClickEvent evt)
+    {
+        _main.style.display = DisplayStyle.Flex;
+        _settings.style.display = DisplayStyle.None;
     }
 
     private void OnQuitClick(ClickEvent evt)
     {
-        //Debug.Log("Quit");
+        Debug.Log("Quit");
         Application.Quit();
     }
 }
