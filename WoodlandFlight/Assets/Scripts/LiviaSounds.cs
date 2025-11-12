c1a3635cef1564416fa48713f07da639d501269e
CheesyRobot <ievalt2020@gmail.com>
2025-11-12 05:06:31 +0200

    Add stepping and flapping sounds

--- WoodlandFlight/Assets/Scripts/LiviaSounds.cs
+++ WoodlandFlight/Assets/Scripts/LiviaSounds.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class LiviaSounds : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private AudioSource source;
+    [SerializeField] private List<AudioClip> walkClips;
+    [SerializeField] private List<AudioClip> flapClips;
+    /*void Awake()
     {
         
-    }
+    }*/
 
     // Update is called once per frame
-    void Update()
+    public void PlayWalkSound()
     {
-        
+        //if (!source.isPlaying)
+        //{
+            //source.clip = audioClips[Random.Range(0, 0)];
+            source.PlayOneShot(walkClips[Random.Range(0,walkClips.Count)]);
+            //source.Play();
+        //}
+    }
+
+    public void PlayFlyingSound()
+    {
+        //source.clip = audioClips[4];
+        source.PlayOneShot(flapClips[Random.Range(0, flapClips.Count)]);
+        //source.Play();
     }
 }
