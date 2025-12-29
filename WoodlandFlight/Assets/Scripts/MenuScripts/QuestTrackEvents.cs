12ca65b539391c2842b0841d4c15e505a35d70ac
CheesyRobot <ievalt2020@gmail.com>
2025-12-29 03:02:18 +0200

    Feat: Quest track UI (Visibility hidden)
    Added Quest Track UI
    The name DialogueUI in Main scene changed to InGameUI, dialogue works as normal
    Created QuestTrackEvents script to control the UI similarly to dialogue UI
    Reorganized some UI textures

---
+++ WoodlandFlight/Assets/Scripts/MenuScripts/QuestTrackEvents.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class QuestTrackEvents : MonoBehaviour
+{
+    private UIDocument _document;
+    private TemplateContainer _questContainer;   // Contains all elements
+    private Label _titleLabel;
+    private Label _textLabel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        // Gets the document that has the UI elements
+        _document = GetComponent<UIDocument>();
+
+        // Getting container
+        _questContainer = _document.rootVisualElement.Q<TemplateContainer>("QuestContainer");
+
+        // Getting labels
+        _titleLabel = _questContainer.Q<Label>("QuestTitle");
+        _textLabel = _questContainer.Q<Label>("QuestText");
+    }
+
+    /// <summary>
+    /// Sets title text
+    /// </summary>
+    /// <param name="text"></param>
+    public void SetTitleText(string text)
+    {
+        _titleLabel.text = text;
+    }
+
+    /// <summary>
+    /// Sets description text
+    /// </summary>
+    /// <param name="text"></param>
+    public void SetDescriptionText(string text)
+    {
+        _textLabel.text = text;
+    }
+
+    /// <summary>
+    /// Hides or shows UI
+    /// </summary>
+    /// <param name="isEnabled"></param>
+    public void Show(bool isEnabled)
+    {
+        if (isEnabled)
+        {
+            _questContainer.style.display = DisplayStyle.Flex;
+        }
+        else
+            _questContainer.style.display = DisplayStyle.None;
+    }
+
+    /// <summary>
+    /// Returns if UI is enabled (showing) or not
+    /// </summary>
+    /// <returns></returns>
+    public bool isEnabled()
+    {
+        if (_questContainer.resolvedStyle.display == DisplayStyle.Flex)
+        {
+            return true;
+        }
+        else return false;
+    }
+}
