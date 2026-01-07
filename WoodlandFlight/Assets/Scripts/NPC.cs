d654995dfa7720598d080c58a58b1ddf23c9fea3
Rocikm <rokassimkunass@gmail.com>
2026-01-07 22:27:55 +0200

    Fix: continue dialogue from a distance

--- WoodlandFlight/Assets/Scripts/NPC.cs
+++ WoodlandFlight/Assets/Scripts/NPC.cs
@@ -11,26 +11,83 @@ public class NPC : MonoBehaviour, IInteractable
     private TalkObjective questDialogue;
     private int currentDialogueLine;
     private Interactor lastInteractor;
+    private bool continueTalking;
     public string InteractionPrompt => prompt;
 
     void Start() {
         currentDialogueLine = 0;
+        continueTalking = false;
     }
 
     void Update() {
         if (lastInteractor != null && Vector3.Distance(lastInteractor.transform.position, transform.position) > exitDistance) {
-            de.Show(false);
-            UICanvas.SetActive(true);
-            lastInteractor = null;
+            StopTalk();
         }
+        if (lastInteractor != null && Input.GetKeyDown(KeyCode.E)) {
+            if (!continueTalking)
+                continueTalking = true;
+            else if (questDialogue == null)
+                ContinueTalkIdle();
+            else
+                ContinueTalkQuest();
+        }
+            
     }
     public bool Interact(Interactor interactor) {
+        if (continueTalking)
+            return true;
         if (questDialogue == null)
-            TalkIdle();
+            StartTalkIdle();
         else
-            TalkQuest();
+            StartTalkQuest();
         lastInteractor = interactor;
+        continueTalking = false;
         return true;
+    }
+    
+    private void StartTalkIdle() {
+        if (!de.isEnabled()) {
+            de.SetNameText(npcName);
+            de.SetDialogueText(dialogue[currentDialogueLine]);
+            de.Show(true);
+            UICanvas.SetActive(false);
+        }
+    }
+
+    public void StartTalkQuest() {
+        if (!de.isEnabled()) {
+            de.SetNameText(npcName);
+            de.SetDialogueText(questDialogue.SayOneLine());
+            de.Show(true);
+            UICanvas.SetActive(false);
+        }
+    }
+
+    private void ContinueTalkIdle() {
+        if (currentDialogueLine < dialogue.Length - 1) {
+            currentDialogueLine++;
+            de.SetDialogueText(dialogue[currentDialogueLine]);
+        }
+        else {
+            StopTalk();
+        }
+    }
+
+    private void ContinueTalkQuest() {
+        if (questDialogue.HasLinesLeft()) {
+            de.SetDialogueText(questDialogue.SayOneLine());
+        }
+        else {
+            questDialogue.SayOneLine();
+            StopTalk();
+        }
+    }
+
+    private void StopTalk() {
+        de.Show(false);
+        UICanvas.SetActive(true);
+        lastInteractor = null;
+        continueTalking = false;
     }
 
     private void TalkIdle() {
