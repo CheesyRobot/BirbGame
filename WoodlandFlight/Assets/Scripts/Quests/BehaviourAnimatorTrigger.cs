a42abbccc510d916a0e178c33b9aa7f5aaac4572
CheesyRobot <ievalt2020@gmail.com>
2026-01-09 00:18:22 +0200

    Misc: Witch sits at end, no speedy gliding and no fish by bear

---
+++ WoodlandFlight/Assets/Scripts/Quests/BehaviourAnimatorTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BehaviourAnimatorTrigger : QuestStageBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private string triggerName;
+    public override void DoAction(object sender, Quest.OnStageChangedEventArgs e)
+    {
+        animator.SetTrigger(triggerName);
+    }
+}
